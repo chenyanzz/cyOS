8b1d26ad0d51da41e6649ce7f791c9dbd2a782f1
XiaoYoung <gmyoung@126.com>
2017-12-24 17:26:58 +0800

    继续写，没写完

--- libs/FGUI.cpp
+++ libs/FGUI.cpp
@@ -1,15 +1,23 @@
 #include "FGUI.h"
+#include "printf.h"
 #include "stdlib.h"
 
-char * strcolor[] = 
+char * color[] = 
 {
-    "BLACK", "DARKGRAY", "RED", "LIGHTRED", "TOMATO" , "GREEN", "LIGHTGREEN", "LAWNGREEN", "BLUE", "SKYBLUE", "LIGHTBULE", "ORANGE", "YELLOW", "PURPLE", "PINK" ,"CYAN", "LIGHTCYAN", "WHITE", "LIGHTGRAY", "LIGHTWHITE", //TextColor有可能会打错
+    "BLACK", "DARKGRAY", "RED", "LIGHTRED", "TOMATO" , "GREEN", "LIGHTGREEN", "LAWNGREEN", "BLUE", "SKYBLUE", "LIGHTBLUE", "ORANGE", "YELLOW", "PURPLE", "PINK" ,"CYAN", "LIGHTCYAN", "WHITE", "LIGHTGRAY", "LIGHTWHITE", //TextColor有可能会打错
     "bgBLACK", "bgRED", "bgGREEN", "bgBLUE", "bgORANGE", "bgPURPLE", "bgCYAN", "bgWHITE"//BgColor有可能会打错，P(打错BgColor) < P(打错TextColor)
 };
 
+char colorB [] = 
+{
+    BLACK, DARKGRAY, RED, LIGHTRED, TOMATO , GREEN, LIGHTGREEN, LAWNGREEN, BLUE, SKYBLUE, LIGHTBLUE, ORANGE, YELLOW, PURPLE, PINK ,CYAN, LIGHTCYAN, WHITE, LIGHTGRAY, LIGHTWHITE, //TextColor有可能会打错
+    bgBLACK, bgRED, bgGREEN, bgBLUE, bgORANGE, bgPURPLE, bgCYAN, bgWHITE//BgColor有可能会打错，P(打错BgColor) < P(打错TextColor)
+};
+
+
 void bootScreen(void)
 {
-    char * str = "made by us";
+    char * str = "made by ${ORANGE}us";
     cls();
     setXY((WIDTH - strlen(str))/2, HEIGHT/2);
     //printf(str);
@@ -19,22 +27,14 @@ void bootScreen(void)
 void printColorStr(char* format)
 {
     int i = 0;
+    int j = 0;
     for (i = 0; format[i] != '\0'; i++)
     {
-        if (format[i] != '%')
+        if (format[i] == '$' && format[++i] == '{')
         {
-            setTerminalColor(WHITE, bgBLACK, false);//默认
-            printChar(format[i]);
+            continue;
         }
-        else
-        {
-            if(format[i++] == '%')
-            {
-                printChar('%');
-            }
-            else
-            {
-            }
-        }
+        setTerminalColor(WHITE, bgBLACK, false);//默认
+        printChar(format[i]);
     }
 }
